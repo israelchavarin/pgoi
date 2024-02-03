407a05707716a44cdb339a12f8440f6e104ac62c
israelchavarin <israelchavarin2d@gmail.com>
2024-02-03 10:17:57 -0600

    Hash password, create jwt and set to cookie

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,11 +2,20 @@ import { User } from "../models/User.js";
 import { UserAccess } from "../models/UserAccess.js";
 import { UserBalance } from "../models/UserBalance.js";
 import { sequelize } from "../database/database.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const registerUser = async (req, res) => {
   const t = await sequelize.transaction();
 
+  const { password } = req.body;
+
   try {
+    const passHashed = await bcrypt.hash(password, 10);
+
     const newUser = await User.create(
       {
         ...req.body,
@@ -14,11 +23,11 @@ export const registerUser = async (req, res) => {
       { transaction: t }
     );
 
-    await UserAccess.create(
+    const newUserAccess = await UserAccess.create(
       {
         user_id: newUser.user_id,
         email: req.body.email,
-        password: req.body.password,
+        password: passHashed,
       },
       { transaction: t }
     );
@@ -32,7 +41,23 @@ export const registerUser = async (req, res) => {
 
     await t.commit();
 
-    res.json(newUser);
+    jwt.sign(
+      { id: newUser.user_id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: process.env.JWT_EXPIRATION,
+      },
+      (err, token) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.cookie("token", token);
+          res.json({
+            message: "User created successfully",
+          });
+        }
+      }
+    );
   } catch (error) {
     await t.rollback();
     console.log(error);
