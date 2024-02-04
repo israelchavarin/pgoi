da34bb6d68038385be60a537cab92768fceeb07b
israelchavarin <israelchavarin2d@gmail.com>
2024-02-03 19:10:07 -0600

    Create logout route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { registerUser, login } from "../controllers/auth.controller.js";
+import { registerUser, login, logout } from "../controllers/auth.controller.js";
 
 const router = Router();
 
 router.post("/register-user", registerUser);
 
 router.post("/login", login);
+
+router.post("/logout", logout);
 
 export default router;
