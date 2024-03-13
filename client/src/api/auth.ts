4d7ddc84e6a01416adc243e1395380ef8bb60764
israelchavarin <israelchavarin2d@gmail.com>
2024-03-13 12:17:22 -0600

    Fix error to get in browser the cookie from the backend

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -7,6 +7,10 @@ export async function postRequest(URL = "", data = {}) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
+    credentials: "include",
+    // necessary for the browser to be able to get the cookie set by the backend
+    // may be blocked by CORS policy if in the backend,
+    // the cors config doesn't include the property credentials: true
   });
   return res.json();
 }
